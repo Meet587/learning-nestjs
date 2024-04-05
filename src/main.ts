4bd5b05af3ada09d929f6f83cbca1f6dff1a3b16
Meet Rakholiya <meet@investifyd.com>
2024-04-05 15:42:23 +0530

    add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    app.setGlobalPrefix("api");
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    const options = new DocumentBuilder()
+        .setTitle("API from Nestjs")
+        .setDescription("learning nest js")
+        .setVersion("1.0")
+        .addTag("Backend API");
+    const document = SwaggerModule.createDocument(app, options.build());
+    SwaggerModule.setup("api-docs", app, document);
     app.enableCors();
     await app.listen(3000);
 }
